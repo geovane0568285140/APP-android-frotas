f7f2f61573f757be469b63edf36666d9f77a1f63
geovane0568285140 <geovane08almeida@gmail.com>
2025-11-27 17:21:57 -0400

    added Authorization the of call the method GET - getExtis

--- app/src/main/java/com/example/appfrotas/remote/serviceRetrofit/ExitService.kt
+++ app/src/main/java/com/example/appfrotas/remote/serviceRetrofit/ExitService.kt
@@ -2,10 +2,11 @@ package com.example.appfrotas.remote.serviceRetrofit
 
 import com.example.appfrotas.remote.Entity.ExitEntityRemote
 import retrofit2.http.GET
+import retrofit2.http.Header
 
 interface ExitService {
 
     @GET("exit/get")
-    suspend fun getExtis(): List<ExitEntityRemote>
+    suspend fun getExtis(@Header("Authorization") token: String): List<ExitEntityRemote>
 
 }
