011fb7bdf58ca15b39284dcd90fb54cf402c6d4c
geovane0568285140 <geovane08almeida@gmail.com>
2025-12-05 12:10:16 -0400

    created method POST in CarService

--- app/src/main/java/com/example/appfrotas/ServiceApp/remote/serviceRetrofit/CarService.kt
+++ app/src/main/java/com/example/appfrotas/ServiceApp/remote/serviceRetrofit/CarService.kt
@@ -1,8 +1,11 @@
 package com.example.appfrotas.ServiceApp.remote.serviceRetrofit
 
+import com.example.appfrotas.ServiceApp.remote.DTOs.Request.CarsCreateRequestDto
 import com.example.appfrotas.ServiceApp.remote.DTOs.Response.CarsResponseDto
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 
 interface CarService {
 
@@ -12,4 +15,6 @@ interface CarService {
     @GET("cars/getLastUsedCars")
     suspend fun getLastUsedCars(@Header("Authorization") tokne: String): List<CarsResponseDto>
 
+    @POST("cars/create")
+    suspend fun createCars(@Header("Authorization") token: String, @Body request: CarsCreateRequestDto)
 }
