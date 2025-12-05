052a41beee0000c02581172928e451f775c32b98
geovane0568285140 <geovane08almeida@gmail.com>
2025-12-05 12:06:36 -0400

    added CarsCreateRequestDto

---
+++ app/src/main/java/com/example/appfrotas/ServiceApp/remote/DTOs/Request/CarsCreateRequestDto.kt
@@ -0,0 +1,62 @@
+package com.example.appfrotas.ServiceApp.remote.DTOs.Request
+
+import com.google.gson.annotations.SerializedName
+import java.time.LocalDateTime
+
+data class CarsCreateRequestDto(
+
+    @SerializedName("license_plate")
+    val license_plate: String,
+
+    @SerializedName("model")
+    val model: String,
+
+    @SerializedName("active")
+    val active: Boolean,
+
+    @SerializedName("mark")
+    val mark: String,
+
+    @SerializedName("manufaturing_year")
+    val manufaturing_year: LocalDateTime,
+
+    @SerializedName("model_year")
+    val model_year: LocalDateTime,
+
+    @SerializedName("color")
+    val color: String,
+
+    @SerializedName("category")
+    val category: String,
+
+    @SerializedName("fuel_type")
+    val fuel_type: String,
+
+    @SerializedName("current_mileage")
+    val current_mileage: String,
+
+    @SerializedName("num_crlv")
+    val num_crlv: String,
+
+    @SerializedName("date_licensing")
+    val date_licensing: LocalDateTime,
+
+    @SerializedName("date_maturity_IPVA")
+    val date_maturity_IPVA: String
+)
+
+/*
+    String license_plate,
+        String model,
+        Boolean active,
+        String mark,
+        LocalDateTime manufaturing_year,
+        LocalDateTime model_year,
+        String color,
+        String category,
+        String fuel_type,
+        String current_mileage,
+        String num_crlv,
+        LocalDateTime date_licensing,
+        LocalDateTime date_maturity_IPVA
+*/
